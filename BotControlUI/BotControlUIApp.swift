210344e1b5d882a004a03af3e017930902af3585
Edgardo <smokeshadowedgardo123@gmail.com>
2026-03-19 20:38:59 -0700

    Initial Commit

---
+++ BotControlUI/BotControlUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BotControlUIApp.swift
+//  BotControlUI
+//
+//  Created by Edgardo Ramos on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BotControlUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
